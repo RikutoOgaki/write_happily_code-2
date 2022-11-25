7d99b7375174bac62aee861b5d3f273ee1e3a742
Rikuto <orikutobasuke@gmail.com>
2022-11-25 11:09:27 +0900

    count add

---
+++ ioc/src/btn.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const btn = () => {
+    // const [countup,setcountup] = useState(0)
+    // const [countdown,setcountdown] = useState(10)
+    const [count,setCount] = useState(0)
+
+    // const countChage = () =>{
+
+    //     if()
+    // }
+
+    const conup = () =>{
+        // setcountup(countup+1)
+    }
+    const condown = () =>{
+        // setcountdown(countdown-1)
+    }
+    return (
+    <div>
+        {/* <p>{countup}</p>
+        <p>{countdown}</p> */}
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>up</button>
+        <button onClick={() => setCount(count - 1)}>down</button>
+    </div>
+    )
+}
+
+export default btn
