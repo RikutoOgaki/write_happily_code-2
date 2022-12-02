42918eb7b4030e3b6f3731a399e04d33324df345
Rikuto <orikutobasuke@gmail.com>
2022-12-02 09:33:04 +0900

    React comment add

--- ioc/src/text.jsx
+++ ioc/src/text.jsx
@@ -2,13 +2,19 @@ import React, { useState } from 'react'
 
 function text() {
     const [Txt,setTxt] = useState(false);  
+    // ↑useStateで最初の初期値をfalseに設定
     const Text = () =>{
       setTxt(!Txt);
+      // ↑setTxtにTxtの逆の意味を持った関数を作る
     }
   return (
     <div>
       <p>{Txt ? "Hello" : "Excuse Me"}</p>
+      {/* ↑Txtの簡単なfor文が書かれている
+          左ならtrue。右ならfalse。という扱いになっっている
+      */}
       <button onClick={Text}>Inner</button>
+      {/* ↑ボタンのクリックでTextの関数を動かしている */}
     </div>
   )
 }
