22de0043fee2a01f09e8cf5063585a3372d4a6b5
Rikuto <orikutobasuke@gmail.com>
2023-01-12 10:45:22 +0900

    React js add

---
+++ ioc/src/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+
+import {Home} from "./compo/home"
+
+
+function page() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default page
